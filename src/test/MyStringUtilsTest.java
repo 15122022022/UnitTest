0bbd210aadbed29fe65db4a0698a184920326875
15122022022 <1160585791@qq.com>
2018-01-08 01:31:23 +0800

    UnitTest1

--- src/test/MyStringUtilsTest.java
+++ src/test/MyStringUtilsTest.java
@@ -1,49 +1,54 @@
 package test;
 
-//import static org.junit.Assert.*;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class MyStringUtilsTest {
 
+	@Before
 	public void setUp() throws Exception {
 	}
 
+	@After
 	public void tearDown() throws Exception {
 	}
 
+	@Test
 	public void teststartsWithIgnoreCase() {
 		MyStringUtils sutils = new MyStringUtils();
 		String str1 = "ABC";
 		String str2 = "qwe";
-//		assertTrue(sutils.startsWithIgnoreCase(str1, "a"));
-//		assertTrue(sutils.startsWithIgnoreCase(str2, "q"));
+		assertTrue(sutils.startsWithIgnoreCase(str1, "a"));
+		assertTrue(sutils.startsWithIgnoreCase(str2, "q"));
 		
 	}
 	
+	@Test
 	public void testTokenizeToStringArray() {
 		MyStringUtils sutils = new MyStringUtils();
 		String str1 = "I am a student";
 		String str2 = "hello,word";
 		int temp1 = sutils.tokenizeToStringArray(str1, " ").length;
 		int temp2 = sutils.tokenizeToStringArray(str2, ",").length;
-//        assertEquals(temp1, 4);
-//        assertEquals(temp2, 2);
+        assertEquals(temp1, 4);
+        assertEquals(temp2, 2);
 		
 	}
 	
+	@Test
 	public void testToDelimitedString() {
 		StringUtilsEx sutils = new StringUtilsEx();
 		List<Double> list = new ArrayList<>();
 		list.add(2.3);
 		list.add(6.8);
 		String result = sutils.toDelimitedString(list, " ");
-//		assertEquals(result.length(),3 );
+		assertEquals(result.length(),7 );
 		
 	}
 
